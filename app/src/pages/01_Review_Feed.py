1107eed744bac9a1ecd46d7dd5932d9eade1cca2
Erica Indman <indman.e@northeastern.edu>
2024-11-28 08:38:13 +0100

    working on view reviews page

--- app/src/pages/01_Review_Feed.py
+++ app/src/pages/01_Review_Feed.py
@@ -18,26 +18,14 @@ SideBarLinks()
 st.header('Reviews')
 
 # You can access the session state to make a more customized/personalized app experience
-st.write(f"### Hi, {st.session_state['first_name']}. Please choose a company to view reviews for.")
+st.write(f"### Hi, {st.session_state['first_name']}. Reviews.")
 
-# get the countries from the world bank data
-with st.echo(code_location='above'):
-    countries:pd.DataFrame = wb.get_countries()
-   
-    st.dataframe(countries)
+# change to access database and fill in company options from there
+options = st.multiselect(
+    "Please choose a company to view reviews for",
+    ["Green", "Yellow", "Red", "Blue"],
+    ["Yellow", "Red"],
+)
 
-# the with statment shows the code for this block above it 
-with st.echo(code_location='above'):
-    arr = np.random.normal(1, 1, size=100)
-    test_plot, ax = plt.subplots()
-    ax.hist(arr, bins=20)
-
-    st.pyplot(test_plot)
-
-
-with st.echo(code_location='above'):
-    slim_countries = countries[countries['incomeLevel'] != 'Aggregates']
-    data_crosstab = pd.crosstab(slim_countries['region'], 
-                                slim_countries['incomeLevel'],  
-                                margins = False) 
-    st.table(data_crosstab)
+# change to show the reviews assosiated with selected companies
+st.write("You selected:", options)
