45eb55ca998e3107de35d6d062d3d462c2a2b52d
Kellyn Small <kellynmacsmall@gmail.com>
2024-12-02 19:15:55 -0500

    monitor reviews

--- api/backend/system_admin/system_admin_routes.py
+++ api/backend/system_admin/system_admin_routes.py
@@ -31,6 +31,43 @@ def get_student_information():
     response.status_code = 200
     return response
 
+# get all the reviews
+@systemadmin.route('/students/reviews/<student_id>', methods=['GET'])
+def get_student_reviews(student_id):
+    query = '''
+        SELECT 
+            reviewId,
+            stars,
+            poster,
+            content,
+            likes,
+            createdAt
+        FROM 
+            reviews 
+        WHERE
+            poster = %s
+    '''
+
+    cursor = db.get_db().cursor()
+    cursor.execute(query, (student_id,))
+
+    theData = cursor.fetchall()
+    
+    the_response = make_response(jsonify(theData))
+    the_response.status_code = 200
+
+    return the_response
+
+# Delete any innapropriate reviews
+@systemadmin.route('/monitorReviews/delete_review/<int:review_id>', methods=['DELETE'])
+def monitor_reviews(review_id):
+    cursor = db.get_db().cursor()
+    
+    cursor.execute("DELETE FROM reviews WHERE reviewId = %s", (review_id,)) 
+    db.get_db().commit()
+    
+    return make_response("Review deleted", 200)
+
 # Update the analytics
 @systemadmin.route('/analytics', methods = ['PUT'])
 def update_analytics():
@@ -38,13 +75,3 @@ def update_analytics():
     current_app.logger.info(product_info)
 
     return "Success"
-
-# Delete any innapropriate reviews
-@systemadmin.route('/monitorReviews', methods=['DELETE'])
-def monitor_reviews(review_id):
-    cursor = db.get_db().cursor()
-    
-    cursor.execute("DELETE FROM reviews WHERE reviewId = %s", (review_id,)) 
-    db.get_db().commit()
-    
-    return "Review deleted"
--- app/src/pages/22_Monitor_Reviews.py
+++ app/src/pages/22_Monitor_Reviews.py
@@ -4,7 +4,6 @@ import streamlit as st
 from modules.nav import SideBarLinks
 import requests
 
-st.set_page_config(layout = 'wide')
 
 SideBarLinks()
 
@@ -16,7 +15,7 @@ st.write('\n\n')
 # set up the page
 st.markdown("# Manage My Reviews and Comments")   
 
-options = requests.get(f'http://api:4000/s/sa/monitorReviews').json()
+options = requests.get(f'http://api:4000/sa/monitorReviews/reviews/{1}').json()
 
 ids = []
 
@@ -31,13 +30,13 @@ review_id = st.selectbox('Review To Delete',
 if st.button("Delete", 
             type='primary', 
             use_container_width=True):
-    response = requests.delete(f'http://api:4000/sa/monitorReviews')
+    response = requests.delete(f'http://api:4000/sa/monitorReviews/delete_review/{review_id}')
     
     if response.status_code == 200 or response.status_code == 204:
         st.write('Review deleted successfully!')
     else:
         st.write(f'Delete failed :( {response.status_code}')
 
-ellies_reviews = requests.get(f'http://api:4000/sa/monitorReviews')
+ellies_reviews = requests.get(f'http://api:4000/sa/monitorReviews/reviews/{1}')
 reviews = ellies_reviews.json()
 st.table(reviews)
