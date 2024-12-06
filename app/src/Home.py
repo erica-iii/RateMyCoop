a61b3b3b8c60b78c60688c1950aa29f5f1cfd7b7
Kellyn Small <kellynmacsmall@gmail.com>
2024-12-05 23:52:46 -0500

    Update Home.py

--- app/src/Home.py
+++ app/src/Home.py
@@ -57,25 +57,12 @@ if st.button("Act as Ellie Ryder, Student",
     logger.info("Logging in as Student Persona")
     st.switch_page('pages/00_Student_Home.py')
 
-if st.button('Act as Mohammad, an USAID worker', 
-            type = 'primary', 
-            use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'usaid_worker'
-    st.session_state['first_name'] = 'Mohammad'
-    st.switch_page('pages/10_USAID_Worker_Home.py')
-
 if st.button('Act as Clark Kent, System Administrator', 
             type = 'primary', 
             use_container_width=True):
     st.session_state['authenticated'] = True
     st.session_state['role'] = 'administrator'
     st.session_state['first_name'] = 'SysAdmin'
-    st.switch_page('pages/20_Admin_Home.py')
-
-if st.button('ex', type = 'primary', use_container_width=True):
-    st.session_state['authenticated'] = True
-    st.session_state['role'] = 'prof'
     st.switch_page('pages/20_Admin_Home.py')
 
 if st.button('Act as Hayden Richards, Co-op Advisor',
